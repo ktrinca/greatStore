7aa101b0c685ecc814742479829f528751199851
Yanina Raspo <yraspo@gmail.com>
2012-10-30 12:23:11 -0300

    modified show for categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -15,6 +15,7 @@ class CategoriesController < ApplicationController
   def show
     @category = Category.find(params[:id])
     @products = @category.products
+    @categories = Category.all
 
     respond_to do |format|
       format.html # show.html.erb
