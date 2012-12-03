16f7af560ea21d694b5d626a480976e6fa330a27
Yanina Raspo <yraspo@gmail.com>
2012-12-03 11:05:02 -0300

    modified dashboard index

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,21 +3,29 @@ ActiveAdmin.register_page "Dashboard" do
   menu :priority => 1, :label => proc{ I18n.t("active_admin.dashboard") }
 
   content :title => proc{ I18n.t("active_admin.dashboard") } do
-    #div :class => "blank_slate_container", :id => "dashboard_default_message" do
+    div :class => "blank_slate_container", :id => "dashboard_default_message" do
       #span :class => "blank_slate" do
         #span "Welcome to Active Admin. This is the default dashboard page."
         #small "To add dashboard sections, checkout 'app/admin/dashboards.rb'"
       #end
-    #end
+    end
 
-    section "Ultimos Productos" do
-      table_for Product.order("updated_at desc").limit(5) do
-        column "Producto", :title
-        column "Precio de venta", :sale_price
-        column :updated_at
-        
+    # Here is an example of a simple dashboard with columns and panels.
+    #
+      columns do
+        column do
+          panel "Productos Recientes" do
+            table_for Product.order("updated_at desc").limit(5) do
+            column "Producto" do |product| 
+               link_to product.title, admin_product_path(product)
+            end
+            column "Precio de venta", :sale_price
+            column "Fecha de Actualizacion", :updated_at
+          end
+           
+        end
       end
-      strong { link_to "Ver todos los Productos", admin_products_path }
+
     end
   end # content
 end
